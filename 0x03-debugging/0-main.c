b2e428a7ab5a12019c6f8647b6e0cec80edd773c
Michael <michaelerukusin@gmail.com>
2022-07-07 03:15:50 -0700

    create a file named 0-main.c. This file must test that the function positive_or_negative() gives the correct output when given a case of 0.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,29 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
 #include <stdio.h>
 
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+
+{
+
+	int i;
 
 
-void positive_or_negative(int i);
+	printf("Infinite loop incoming :(\n");
 
-int largest_number(int a, int b, int c);
-
-int convert_day(int month, int day);
-
-void print_remaining_days(int month, int day, int year);
+	i = 0;
 
 
+	/* while (i < 10)*/
+	/* {*/
+	/* putchar(i);*/
+	/* }*/
 
-#endif /* MAIN_H */
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
